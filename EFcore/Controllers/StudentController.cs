16c7823481c986e28c7fd3f8297f96016831f95d
Aayush Bajracharya <aayushbajracharya90@gmail.com>
2025-01-12 12:44:08 +0545

    Base Controller

--- EFcore/Controllers/StudentController.cs
+++ EFcore/Controllers/StudentController.cs
@@ -1,24 +1,22 @@
-﻿using Azure;
-using Domain.Command.Cmd.StudentCmd;
+﻿using Domain.Command.Cmd.StudentCmd;
 using Domain.Command.Queries.StudentsQueries;
 using Domain.DTOs.StudentDTOs;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace EFcore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StudentController : ControllerBase
+    public class StudentController : BaseApiController
     {
-        private readonly IMediator _mediator;
+        //private readonly IMediator _mediator;
         private readonly ILogger<StudentController> _logger;
 
 
         public StudentController(IMediator mediator, ILogger<StudentController> logger, IWebHostEnvironment webHostEnvironment)
         {
-            _mediator = mediator;
+            //_mediator = mediator;
             _logger = logger;
         }
 
@@ -26,7 +24,7 @@ namespace EFcore.Controllers
         public async Task<IActionResult> GetAllStudentAsync()
         {
             _logger.LogInformation($"{nameof(GetAllStudentAsync)} trigger function received a request with param query");
-            var students = await _mediator.Send(new GetAllStudentsQuery());
+            var students = await Mediator.Send(new GetAllStudentsQuery());
             _logger.LogInformation($"{nameof(GetAllStudentAsync)}  trigger function returned a response");
             return Ok(students);
         }
@@ -36,7 +34,7 @@ namespace EFcore.Controllers
         public async Task<IActionResult> AddStudentAsync(CreateStudentDTO student)
         {
             _logger.LogInformation($"{nameof(AddStudentAsync)} trigger function received a request with param query");
-            var id = await _mediator.Send(new CreateStudentCommand(student));
+            var id = await Mediator.Send(new CreateStudentCommand(student));
             _logger.LogInformation($"{nameof(AddStudentAsync)}  trigger function returned a response");
             return Ok(id);
         }
@@ -45,7 +43,7 @@ namespace EFcore.Controllers
         public async Task<IActionResult> GetStudentByIdAsync(int id)
         {
             _logger.LogInformation($"{nameof(GetStudentByIdAsync)} trigger function received a request with param query");
-            var getById = await _mediator.Send(new GetStudentsByIdQuery(id));
+            var getById = await Mediator.Send(new GetStudentsByIdQuery(id));
             _logger.LogInformation($"{nameof(GetStudentByIdAsync)}  trigger function returned a response");
             return getById != null ? Ok(getById) : NotFound();
         }
